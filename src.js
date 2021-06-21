15bd3277ca86f4652e57e0101fef4e33db485c03
navneetprajapati26 <85722269+navneetprajapati26@users.noreply.github.com>
2021-06-21 15:21:34 +0530

    first

---
+++ src.js
@@ -0,0 +1,7 @@
+function clicked(){
+    let para = document.getElementById('para');
+    let btn = document.getElementById('btn');
+    //para.style.display = 'none';
+    document.querySelectorAll('.username')[0].innerHTML = "<b> our l@@@ lalit kitana  kiye the aagye muh utha ke result dekhane, chal nikal tera back laga he 6 subject me </b>"
+    
+}
