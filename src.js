95b8fdd3a02d6f09ca2d985db94796d4a48d8fb8
navneetprajapati26 <85722269+navneetprajapati26@users.noreply.github.com>
2021-06-21 15:51:18 +0530

    ss

--- src.js
+++ src.js
@@ -2,6 +2,6 @@ function clicked(){
     let para = document.getElementById('para');
     let btn = document.getElementById('btn');
     //para.style.display = 'none';
-    document.querySelectorAll('.username')[0].innerHTML = "<b> our l@@@ lalit kitana  kiye the aagye muh utha ke result dekhane, chal nikal tera back laga he 6 subject me  bsdk padhai likhai kro ... syllabus ka pta nhi results dekhne aa jate hai bar bar Itna smy kisi ldki ko dete to wo set ho jati par nhi enhe results chahiye </b>"
+    document.querySelectorAll('.username')[0].innerHTML = "<b>bsdk padhai likhai kro ... syllabus ka pta nhi results dekhne aa jate hai bar bar Itna smy kisi ldki ko dete to wo set ho jati par nhi enhe results chahiye </b>"
     
 }
